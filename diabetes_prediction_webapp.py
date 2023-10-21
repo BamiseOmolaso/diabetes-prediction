02491a05b69f75b145cf9552cd854712bfdd29d9
BamiseOmolaso <davidbams3@gmail.com>
2023-10-21 21:31:50 +0100

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-with open('model_diabetesprediction.pkl', 'rb') as file:
+with open('/model_diabetesprediction.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
     
     
